2ef4659741cc802ca4ee268733b6050c72d1cfe1
Kimmy Leo <kenpusney@gmail.com>
2016-09-18 00:36:38 +0800

    convert to standalone repo

---
+++ test/TestSuite.cpp
@@ -0,0 +1,3 @@
+#include "../include/unittest.h"
+
+TestRunner();
---
+++ test/tpl_func_test.cpp
@@ -0,0 +1,10 @@
+
+#include "../include/unittest.h"
+#include "../include/tpl/debug.h"
+
+#include "../include/tpl/func.h"
+
+TestCase(tpl_func){
+
+}
+
